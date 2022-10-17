74f6a86dfce512e04310c5185315249dbcd261dc
Emmanuel Aquino <luisbert_aquino@hotmail.com>
2022-10-17 16:34:06 -0400

    Quizbrain and main function working

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
+# This a list of question objects
 question_bank = []
 
 for q in question_data:
     new_q = Question(q['text'], q['answer'])
     question_bank.append(new_q)
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.q_number}")
