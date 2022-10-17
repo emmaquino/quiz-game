08db2c6cb63c182f40e2af70247e2d44f5185cff
Emmanuel Aquino <luisbert_aquino@hotmail.com>
2022-10-17 15:47:52 -0400

    Defining question bank

---
+++ main.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for q in question_data:
+    new_q = Question(q['text'], q['answer'])
+    question_bank.append(new_q)
+
